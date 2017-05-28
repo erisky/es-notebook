21bbce1fe94d37b81de8b5f5fe081e03d45af967
Eric Chen <frederick.chen9@gmail.com>
2017-05-29 01:07:24 +0800

    Update cmake: tutorial for cpack using : cpack --config CPackConfig.cmake

--- cmake/tutorial/TutorialConfig.h
+++ cmake/tutorial/TutorialConfig.h
@@ -4,3 +4,6 @@
 #define Tutorial_VERSION_MAJOR 1
 #define Tutorial_VERSION_MINOR 2
 
+/* #undef HAVE_LOG */
+/* #undef HAVE_EXP */
+#define HAVE_PRINTF
--- cmake/tutorial/tutorial.c
+++ cmake/tutorial/tutorial.c
@@ -5,12 +5,19 @@
 #include "my_sqrt.h"
 #endif
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int x=9;
+    int x = 9;
 
     printf("Release version: %d.%d \n",Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
     printf("If this work\n");
+
+#ifdef HAVE_PRINTF
+    printf("Printf is available\n");
+#else
+    exit(-1);
+#endif
+
 #ifdef USE_MYSQRT
     printf("MY SQRT:%d = %d", x, my_sqrt(x));
 #else
